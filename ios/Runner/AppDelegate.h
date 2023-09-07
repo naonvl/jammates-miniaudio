83d190fd66c8f767721813f3af70d9a182f74170
nao <chiwoo@postech.ac.kr>
2023-09-07 13:36:19 +0700

    remove module

--- ios/Runner/AppDelegate.h
+++ ios/Runner/AppDelegate.h
@@ -1,6 +1,10 @@
-#import <Flutter/Flutter.h>
-#import <UIKit/UIKit.h>
+// Copyright 2014 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-@interface AppDelegate : FlutterAppDelegate
+#import <UIKit/UIKit.h>
+#import <Flutter/Flutter.h>
+
+@interface AppDelegate : FlutterAppDelegate<FlutterStreamHandler>
 
 @end
